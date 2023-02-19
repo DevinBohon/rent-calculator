1fb3972d233b528648af1e3a73d0695482edc01e
DevinBohon <100527186+DevinBohon@users.noreply.github.com>
2023-02-19 16:35:37 -0500

    Update can i afford rent.js

--- can i afford rent with this rate/can i afford rent.js
+++ can i afford rent with this rate/can i afford rent.js
@@ -1,6 +1,6 @@
 //this is to calculate to see if an hourly rate is able afford an apartment.
 //n is the hourly rate
-//2080 is the total number of hours worked a year if you work a 40 hour work week
+//2080 is the total number of hours worked a year if you work a 40 hour work week and is based on an 80 hour pay period
 //divided by 12 for the months of the year
 // divided by three due to appartment having a policy of tenants being required 3* rent in a  month for them to even rent out to the tenant
 function rent (n) {
